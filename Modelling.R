36198c779139ff76b16e8ac3d8e81062cd9902bf
nikitabonawate <nikita75@ymail.com>
2016-09-10 10:54:05 -0400

    Update Modelling.R

--- Modelling.R
+++ Modelling.R
@@ -1,13 +1,18 @@
 library(forecast)
 library(plotly)
 library(tsoutliers)
+
+# the below file contains all fuctions of modelling
 source("modelling_function.R")
 debug(modelling_function)
 
+#Initial data cleaning is done in Excel, so that only relevant records are being fetched from the Sales data file
 sales <- read.csv("SalesWithCRD_1.csv", header= TRUE)
+
+#if delivery_qty= 0, remove those records from analysis
 sales <- sales[(sales$STATUS== "IN" & sales$DELIVERY_QTY_GB_EQUIVS>0),]
 
-# trying to get the product_groups having all 31 months present
+# get the product_groups which has at least 2 years of data avaialble in the past
 sales$SHIP_FISCAL_PERIOD_SORT <- as.factor(sales$SHIP_FISCAL_PERIOD_SORT)
 sales_monthly <-aggregate(formula= DELIVERY_QTY_GBYTE_EQUIVS ~ SHIP_FISCAL_PERIOD_SORT+ PRODUCT_GROUP,data=sales,FUN=sum)
 uniquecount(sales_monthly$PRODUCT_GROUP)
@@ -18,6 +23,7 @@ uniquecount(sales_monthly$PRODUCT_GROUP)
 
 for(i in 1:2)
   {
+    #Creating time series data
     sales_new <- sales_monthly[which(sales_monthly$PRODUCT_GROUP == data$x[i]),]
     sales_new <- sales_new[order(sales_new$SHIP_FISCAL_PERIOD_SORT),]
     sales_new$SHIP_FISCAL_PERIOD_SORT <- NULL
@@ -26,25 +32,26 @@ for(i in 1:2)
     #Calender month conversion based on Fiscal Calender basis
     myts <- ts(data= sales_new,start=c(2013,12), end=c(2016,07), frequency=12)
     time(myts)
+    
+    # remove the calendar effects from the data
     ce <- calendar.effects(myts,trading.day = TRUE)
+    
+    #divide the data into training and test data set
     mytsTrain <- window(myts,end= 2015.833)
     mytsTest <- window(myts,start = 2015.917)
     forecastmonths = 3
     trainPoints= 24
+    
+    # run the models
     a1 <-modelling_function(mytsTrain,mytsTest,"arima",forecastmonths,trainPoints) #
     a2 <-modelling_function(mytsTrain,mytsTest,"ets",forecastmonths,trainPoints) #Simple Exponential Smoothing
-    # a3 <-modelling_function(mytsTrain,mytsTest,"Mean",forecastmonths,trainPoints)
-    # a4 <-modelling_function(mytsTrain,mytsTest,"Random Walk Forest",forecastmonths,trainPoints)
-    # a5 <-modelling_function(mytsTrain,mytsTest,"Random Walk Forest with Drift",forecastmonths,trainPoints)
-    # a6 <-modelling_function(mytsTrain,mytsTest,"Croston",forecastmonths,trainPoints)
-    #a7 <-modelling_function(mytsTrain,mytsTest,"Spline",forecastmonths,trainPoints)
+    a3 <-modelling_function(mytsTrain,mytsTest,"Mean",forecastmonths,trainPoints)
+    a4 <-modelling_function(mytsTrain,mytsTest,"Random Walk Forest",forecastmonths,trainPoints)
+    a5 <-modelling_function(mytsTrain,mytsTest,"Random Walk Forest with Drift",forecastmonths,trainPoints)
+    a6 <-modelling_function(mytsTrain,mytsTest,"Croston",forecastmonths,trainPoints)
+    a7 <-modelling_function(mytsTrain,mytsTest,"Spline",forecastmonths,trainPoints)
     a8 <-modelling_function(mytsTrain,mytsTest,"tbats",forecastmonths,trainPoints)
-    #a1
-    #a2
-    #a3
-    #a4
-    #a5
-    #a6
+    
 }
 averageError <- sumval/nrow(data)
 print(averageError)
